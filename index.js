521c0981b13bafa3e487d0c75ef748a8267fa18a
nDamt <N00242439@upn.pe>
2025-06-19 01:17:29 -0500

    Add slash command registration and fix bot online message

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits} = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder} = require('discord.js');
+const { Player } = require('discord-player');
 
 const client = new Client({
     intents:[
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates
     ]
 })
+
+const player = new Player(client);
+
 client.once('ready', () => {
-    console.log("Bot is online ${client.user.tag})");
+    console.log(`Bot is online ${client.user.tag}`);
+    registerCommands();
 });
 
-cliente.login(process.env.TOKEN);
+//CREO LOS SLASH PARA PODER HACER USO DE LOS COMMANDOS
+const commands = [
+    new SlashCommandBuilder()
+        .setName('play')
+        .setDescription('Reproducir música')
+        .addStringOption(option => 
+            option.setName('query').setDescription('Capo dame el nombre o link de la canción').setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('pause')
+        .setDescription('Pausar la música'),
+    new SlashCommandBuilder()
+        .setName('resume')
+        .setDescription('Reanudar la música'),
+    new SlashCommandBuilder()
+        .setName('skip')
+        .setDescription('Saltar la música actual'),
+    new SlashCommandBuilder()
+        .setName('stop')
+        .setDescription('Detener la música')
+].map(command => command.toJSON());    // Esto sirve para convertir los comandos a JSON vale :)
+
+async function registerCommands(){
+    const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+    try{
+        console.log('🔄 Registrando Slash Commands . . . .')
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        ),
+        console.log('✅ Slash Commands registrados ;)');
+    }catch (error){
+        console.error('❌ Error al registrar los comandos:', error);
+    }
+}
+
+client.login(process.env.TOKEN);
